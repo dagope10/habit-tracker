61e9a4d72cac82c115b31cb02112bd5fdf8fbd7e
dagope10 <147617341+dagope10@users.noreply.github.com>
2024-04-12 17:00:54 +0200

    Delete habit-tracker/backend directory

--- habit-tracker/backend/src/main/java/com/proyecto/habittracker/repository/CategoriaRepository.java
+++
@@ -1,9 +0,0 @@
-package com.proyecto.habittracker.repository;
-
-import com.proyecto.habittracker.entities.Categoria;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface CategoriaRepository extends JpaRepository<Categoria, Integer> {
-}
